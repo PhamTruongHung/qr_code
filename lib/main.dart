4764bc3bbe90e0213cdf622ac9217751aae3e805
trhung <trh.bkdn>
2019-07-10 01:31:49 +0700

    - Chay duoc, trong video khong  chay, nhung minh chay duoc. :3 Khong hieu - Quet ra duoc noi dung dung, da test

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,48 @@
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
+import 'package:flutter/services.dart';
 
 void main() => runApp(MaterialApp(
   debugShowCheckedModeBanner: false,
   home: HomePage(),
 ));
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String result = "Hey there!";
+  Future _scanQR() async {
+    try{
+      String qrResult = await BarcodeScanner.scan();
+      setState(() {
+        result = qrResult;
+      });
+    }on PlatformException catch (ex){
+      if(ex.code == BarcodeScanner.CameraAccessDenied){
+        setState(() {
+          result = "Camera permission was denied";
+        });
+      } else {
+        setState(() {
+          result = "$ex Wrong!!!";
+        });
+      }
+    }on FormatException{
+      setState(() {
+        result = "Press back btn before!!!";
+      });
+    }catch(ex){
+      setState(() {
+        result = "$ex Wrong!!!";
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -15,16 +51,14 @@ class HomePage extends StatelessWidget {
       ),
       body: Center(
         child: Text(
-          "Hey there ! ",
+          result,
           style: new TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
         ),
       ),
       floatingActionButton: FloatingActionButton.extended(
         icon: Icon(Icons.camera_alt),
         label: Text("Scan"),
-        onPressed: (){
-          //TODO: action when btn pressed
-        },
+        onPressed: _scanQR,
       ),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
